091addf66e85f9c3684ed3aeccc644e5e5e5ae12
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2022-04-08 18:16:37 +0300

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -3,18 +3,35 @@ from translation import Translation
 from pyrogram import Client, filters
 from database.add import add_user_to_database
 from functions.forcesub import handle_force_subscribe
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-@Client.on_message(filters.private & filters.command(["start", "help", "ayarlar", "settings"]))
-async def start(bot, update):
-    if not update.from_user:
-        return await update.reply_text("Seni tanımıyorum ahbap.")
-    await add_user_to_database(bot, update)
+@Client.on_message(filters.private & filters.command(["start", "help"]))
+async def start(bot: Client, event: Message):
+    if not event.from_user:
+        return await event.reply_text("Seni tanımıyorum ahbap.")
+    await add_user_to_database(bot, event)
     if AUTH_CHANNEL:
-        fsub = await handle_force_subscribe(bot, update)
+        fsub = await handle_force_subscribe(bot, event)
         if fsub == 400:
             return
-    await update.reply_text(
-        text=Translation.START_TEXT.format(update.from_user.mention),
+    await event.reply_text(
+        text=Translation.START_TEXT.format(event.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=Translation.START_BUTTONS
+    )
+
+@Client.on_message(filters.private & filters.command(["ayarlar", "settings"]))
+async def delete_thumb_handler(bot: Client, event: Message):
+    if not event.from_user:
+        return await event.reply_text("Seni tanımıyorum ahbap.")
+    await add_user_to_database(bot, event)
+    if AUTH_CHANNEL:
+        fsub = await handle_force_subscribe(bot, event)
+        if fsub == 400:
+            return
+    await event.reply_text(
+        Translation.SETTİNGS,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("⚙ Ayarlar", callback_data="Settings")]
+        ])
     )
