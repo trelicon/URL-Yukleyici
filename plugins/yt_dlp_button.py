858dcff719e7495b2afff6759739721c39001c06
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2022-04-08 17:30:11 +0300

    Update yt_dlp_button.py

--- plugins/yt_dlp_button.py
+++ plugins/yt_dlp_button.py
@@ -286,7 +286,7 @@ async def yt_dlp_call_back(bot, update):
                     thumb=thumbnail,
                     caption=custom_file_name,
                     reply_to_message_id=update.message.reply_to_message.message_id,
-                    reply_markup=reply_markup,
+                    # reply_markup=reply_markup,
                     progress=progress_for_pyrogram,
                     progress_args=(
                         Translation.UPLOAD_START,
@@ -308,7 +308,7 @@ async def yt_dlp_call_back(bot, update):
                     width=width,
                     height=height,
                     supports_streaming=True,
-                    reply_markup=reply_markup,
+                    # reply_markup=reply_markup,
                     thumb=thumb_image_path,
                     reply_to_message_id=update.message.reply_to_message.message_id,
                     progress=progress_for_pyrogram,
@@ -334,7 +334,7 @@ async def yt_dlp_call_back(bot, update):
                     duration=duration,
                     thumb=thumbnail,
                     reply_to_message_id=update.message.reply_to_message.message_id,
-                    reply_markup=reply_markup,
+                    # reply_markup=reply_markup,
                     progress=progress_for_pyrogram,
                     progress_args=(
                         Translation.UPLOAD_START,
@@ -355,7 +355,7 @@ async def yt_dlp_call_back(bot, update):
                     length=width,
                     thumb=thumbnail,
                     reply_to_message_id=update.message.reply_to_message.message_id,
-                    reply_markup=reply_markup,
+                    # reply_markup=reply_markup,
                     progress=progress_for_pyrogram,
                     progress_args=(
                         Translation.UPLOAD_START,
