ce75cc53e7455e5a19ac208be1d622aa877758f0
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2022-04-08 17:21:19 +0300

    Create log.py

---
+++ plugins/log.py
@@ -0,0 +1,20 @@
+from config import OWNER_ID
+from pyrogram import Client, filters
+
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
+                    level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+
+@Client.on_message(filters.command('log') & filters.user(OWNER_ID))
+async def sendLogs(client, message):
+    with open('log.txt', 'rb') as f:
+        try:
+            await client.send_document(document=f,
+                                       file_name=f.name, reply_to_message_id=message.message_id,
+                                       chat_id=message.chat.id, caption=f.name)
+        except Exception as e:
+            await message.reply_text(str(e))
