5fc1f1687c6d6f70785ec0ed048af25b5994558f
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2022-04-08 17:23:45 +0300

    Update yt_dlp_echo.py

--- plugins/yt_dlp_echo.py
+++ plugins/yt_dlp_echo.py
@@ -1,9 +1,9 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
+    level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
 
 import os, time, asyncio, json
 
@@ -17,7 +17,6 @@ from functions.display_progress import humanbytes
 from functions.help_uploadbot import DownLoadFile
 from functions.forcesub import handle_force_subscribe
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
 
 @Client.on_message(filters.private & filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
@@ -38,22 +37,23 @@ async def echo(bot, update):
             print(error)
     if not update.from_user:
         return await update.reply_text("Seni tanımıyorum ahbap.")
-    if AUTH_CHANNEL:
-        fsub = await handle_force_subscribe(bot, update)
-        if fsub == 400:
-            return
     await add_user_to_database(bot, update)
     await bot.send_chat_action(
         chat_id=update.chat.id,
         action="typing"
     )
-    logger.info(update.from_user)
+    if AUTH_CHANNEL:
+        fsub = await handle_force_subscribe(bot, update)
+        if fsub == 400:
+            return
+    LOGGER.info(update.from_user)
     url = update.text
     yt_dlp_username = None
     yt_dlp_password = None
     file_name = None
 
-    print(url)
+    random = str(time.time())
+    LOGGER.info(url)
     if "|" in url:
         url_parts = url.split("|")
         if len(url_parts) == 2:
@@ -80,8 +80,8 @@ async def echo(bot, update):
             yt_dlp_username = yt_dlp_username.strip()
         if yt_dlp_password is not None:
             yt_dlp_password = yt_dlp_password.strip()
-        logger.info(url)
-        logger.info(file_name)
+        LOGGER.info(url)
+        LOGGER.info(file_name)
     else:
         for entity in update.entities:
             if entity.type == "text_link":
@@ -124,7 +124,7 @@ async def echo(bot, update):
     if yt_dlp_password is not None:
         command_to_exec.append("--password")
         command_to_exec.append(yt_dlp_password)
-    logger.info(command_to_exec)
+    LOGGER.info(command_to_exec)
     send_message = await bot.send_message(
         chat_id=update.chat.id,
         text=f"İşleniyor...⏳",
@@ -140,12 +140,14 @@ async def echo(bot, update):
     # Wait for the subprocess to finish
     stdout, stderr = await process.communicate()
     e_response = stderr.decode().strip()
-    # logger.info(e_response)
+    # LOGGER.info(e_response)
     t_response = stdout.decode().strip()
-    # logger.info(t_response)
+    # LOGGER.info(t_response)
     if e_response and "nonnumeric port" not in e_response:
-        # logger.warn("Status : FAIL", exc.returncode, exc.output)
-        error_message = e_response.replace("please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output.", "")
+        # LOGGER.warn("Status : FAIL", exc.returncode, exc.output)
+        error_message = e_response.replace(
+            "please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output.",
+            "")
         if "This video is only available for registered users." in error_message:
             error_message += Translation.SET_CUSTOM_USERNAME_PASSWORD
         time.sleep(1)
@@ -166,7 +168,6 @@ async def echo(bot, update):
         else:
             response_json.append(json.loads(x_reponse))
         # response_json = json.loads(x_reponse)
-        random = str(time.time())
         save_ytdl_json_path = os.path.join(
             DOWNLOAD_LOCATION,
             str(update.from_user.id) + random + ".json"
@@ -219,11 +220,11 @@ async def echo(bot, update):
                             )"""
                     else:
                         ikeyboard = [
-                                InlineKeyboardButton(
-                                    dipslay_str_uon,
-                                    callback_data=(cb_string_video).encode("UTF-8"),
-                                )
-                            ]
+                            InlineKeyboardButton(
+                                dipslay_str_uon,
+                                callback_data=(cb_string_video).encode("UTF-8"),
+                            )
+                        ]
                     inline_keyboard.append(ikeyboard)
                 if duration is not None:
                     cb_string_64 = "{}|{}|{}|{}".format("audio", "64k", "mp3", random)
@@ -249,48 +250,62 @@ async def echo(bot, update):
                             )
                         ]
                     )
+                    inline_keyboard.append([
+                        InlineKeyboardButton(
+                            "✔️ Kapat", callback_data='close')
+                    ])
             else:
                 format_id = current_r_json["format_id"]
                 format_ext = current_r_json["ext"]
-                cb_string_video = "{}|{}|{}|{}".format("video", format_id, format_ext, random)
+                cb_string_video = "{}={}={}={}".format("video", format_id, format_ext, random)
                 inline_keyboard.append(
                     [
                         InlineKeyboardButton(
-                            "🗂 Document", callback_data=(cb_string_video).encode("UTF-8")
+                            "🗂 File", callback_data=(cb_string_video).encode("UTF-8")
                         )
                     ]
                 )
             break
-        inline_keyboard.append([InlineKeyboardButton("✖️ Kapat", callback_data='close')])
+        reply_markup = InlineKeyboardMarkup(inline_keyboard)
+    else:
+        # fallback for nonnumeric port a.k.a seedbox.io
+        inline_keyboard = []
+        cb_string_video = "{}={}={}={}".format("video", "OFL", "ENON", random)
+        inline_keyboard.append([
+            InlineKeyboardButton(
+                "🗂 File",
+                callback_data=(cb_string_video).encode("UTF-8")
+            )
+        ])
         reply_markup = InlineKeyboardMarkup(inline_keyboard)
         # logger.info(reply_markup)
-        thumbnail = DEF_THUMB_NAIL_VID_S
-        thumbnail_image = DEF_THUMB_NAIL_VID_S
+    thumbnail = DEF_THUMB_NAIL_VID_S
+    thumbnail_image = DEF_THUMB_NAIL_VID_S
 
-        if "thumbnail" in current_r_json:
-            if current_r_json["thumbnail"] is not None:
-                thumbnail = current_r_json["thumbnail"]
-                thumbnail_image = current_r_json["thumbnail"]
-        thumb_image_path = DownLoadFile(
-            thumbnail_image,
-            DOWNLOAD_LOCATION + "/" +
-            str(update.from_user.id) + random + ".webp",
-            CHUNK_SIZE,
-            None,  # bot,
-            Translation.DOWNLOAD_START,
-            update.message_id,
-            update.chat.id
-        )
+    if "thumbnail" in current_r_json:
+        if current_r_json["thumbnail"] is not None:
+            thumbnail = current_r_json["thumbnail"]
+            thumbnail_image = current_r_json["thumbnail"]
+    thumb_image_path = DownLoadFile(
+        thumbnail_image,
+        DOWNLOAD_LOCATION + "/" +
+        str(update.from_user.id) + random + ".webp",
+        CHUNK_SIZE,
+        None,  # bot,
+        Translation.DOWNLOAD_START,
+        update.message_id,
+        update.chat.id
+    )
 
-        if os.path.exists(thumb_image_path):
-            im = Image.open(thumb_image_path).convert("RGB")
-            im.save(thumb_image_path.replace(".webp", ".jpg"), "jpeg")
-        else:
-            thumb_image_path = None
+    if os.path.exists(thumb_image_path):
+        im = Image.open(thumb_image_path).convert("RGB")
+        im.save(thumb_image_path.replace(".webp", ".jpg"), "jpeg")
+    else:
+        thumb_image_path = None
 
-        await send_message.edit_text(
-            text=Translation.FORMAT_SELECTION.format(thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
-            reply_markup=reply_markup,
-            parse_mode="html",
-            disable_web_page_preview=True
-        )
+    await send_message.edit_text(
+        text=Translation.FORMAT_SELECTION.format(thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
+        reply_markup=reply_markup,
+        parse_mode="html",
+        disable_web_page_preview=True
+    )
