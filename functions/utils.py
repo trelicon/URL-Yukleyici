c732e950ad403da85560778c210f829b725c775f
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2022-04-08 17:22:14 +0300

    Update utils.py

--- functions/utils.py
+++ functions/utils.py
@@ -35,3 +35,21 @@ def remove_emoji(string):
                                u"\u3030"
                                "]+", flags=re.UNICODE)
     return emoji_pattern.sub(r'', string)
+
+def ReadableTime(seconds: int) -> str:
+    result = ''
+    (days, remainder) = divmod(seconds, 86400)
+    days = int(days)
+    if days != 0:
+        result += f'{days}d'
+    (hours, remainder) = divmod(remainder, 3600)
+    hours = int(hours)
+    if hours != 0:
+        result += f'{hours}h'
+    (minutes, seconds) = divmod(remainder, 60)
+    minutes = int(minutes)
+    if minutes != 0:
+        result += f'{minutes}m'
+    seconds = int(seconds)
+    result += f'{seconds}s'
+    return result
