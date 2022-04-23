cd3f48ff5b07422e30b5f9702eff9d193c9c6b70
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2022-04-23 16:54:43 +0300

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -7,7 +7,7 @@ from plugins.settings.settings import Settings
 from functions.forcesub import handle_force_subscribe
 
 
-@Client.on_message(filters.private & filters.command(["start", "help"]))
+@Client.on_message(filters.incoming & filters.command(["start", "help"]))
 async def start(bot: Client, event: Message):
     if not event.from_user:
         return await event.reply_text("Seni tanımıyorum ahbap.")
@@ -19,10 +19,11 @@ async def start(bot: Client, event: Message):
     await event.reply_text(
         text=Translation.START_TEXT.format(event.from_user.mention),
         disable_web_page_preview=True,
+        reply_to_message_id=event.message_id,
         reply_markup=Translation.START_BUTTONS
     )
 
-@Client.on_message(filters.private & filters.command(["ayarlar", "settings"]))
+@Client.on_message(filters.incoming & filters.command(["ayarlar", "settings"]))
 async def delete_thumb_handler(bot: Client, event: Message):
     if not event.from_user:
         return await event.reply_text("Seni tanımıyorum ahbap.")
