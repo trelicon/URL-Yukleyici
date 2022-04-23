4617777e6cb714f2b4793b41d24c95f7b585f7ce
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2022-04-23 17:38:14 +0300

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -12,8 +12,10 @@ from translation import Translation
 from pyrogram import Client, filters
 from plugins.settings.settings import Settings
 
+
 @Client.on_callback_query()
 async def button(bot, update):
+    user_id = update.from_user.id
     if update.data == "home":
         await update.answer()
         await update.message.edit_text(
@@ -32,7 +34,7 @@ async def button(bot, update):
         await update.answer()
         await Settings(update.message)
     elif update.data == "showThumbnail":
-        thumbnail = await db.get_thumbnail(update.from_user.id)
+        thumbnail = await db.get_thumbnail(user_id)
         if not thumbnail:
             await update.answer("Herhangi bir thumbnail ayarlamadınız!", show_alert=True)
         else:
@@ -43,51 +45,59 @@ async def button(bot, update):
                                                               callback_data="deleteThumbnail")
                                ]]))
     elif update.data == "deleteThumbnail":
-        await db.set_thumbnail(update.from_user.id, None)
+        await db.set_thumbnail(user_id, None)
         await update.answer("Başarıyla silindi.", show_alert=True)
         await update.message.delete(True)
     elif update.data == "setThumbnail":
         await update.answer(Translation.THUMBNAIL_TEXT, show_alert=True)
     elif update.data == "triggerGenSS":
         await update.answer()
-        generate_ss = await db.get_generate_ss(update.from_user.id)
+        generate_ss = await db.get_generate_ss(user_id)
         if generate_ss:
-            await db.set_generate_ss(update.from_user.id, False)
+            await db.set_generate_ss(user_id, False)
         else:
-            await db.set_generate_ss(update.from_user.id, True)
+            await db.set_generate_ss(user_id, True)
         await Settings(update.message)
     elif update.data == "triggerGenSample":
         await update.answer()
-        generate_sample_video = await db.get_generate_sample_video(update.from_user.id)
+        generate_sample_video = await db.get_generate_sample_video(user_id)
         if generate_sample_video:
-            await db.set_generate_sample_video(update.from_user.id, False)
+            await db.set_generate_sample_video(user_id, False)
         else:
-            await db.set_generate_sample_video(update.from_user.id, True)
+            await db.set_generate_sample_video(user_id, True)
         await Settings(update.message)
     elif update.data == "setCaption":
         await update.answer()
-        caption = await db.get_caption(update.from_user.id)
+        caption = await db.get_caption(user_id)
         if caption:
-            await db.set_caption(update.from_user.id, False)
+            await db.set_caption(user_id, False)
         else:
-            await db.set_caption(update.from_user.id, True)
+            await db.set_caption(user_id, True)
+        await Settings(update.message)
+    elif update.data == "aria2":
+        await update.answer()
+        aria2 = await db.get_aria2(user_id)
+        if aria2:
+            await db.set_aria2(user_id, False)
+        else:
+            await db.set_aria2(user_id, True)
         await Settings(update.message)
     elif update.data == "triggerUploadMode":
         await update.answer()
-        upload_as_doc = await db.get_upload_as_doc(update.from_user.id)
+        upload_as_doc = await db.get_upload_as_doc(user_id)
         if upload_as_doc:
-            await db.set_upload_as_doc(update.from_user.id, False)
+            await db.set_upload_as_doc(user_id, False)
         else:
-            await db.set_upload_as_doc(update.from_user.id, True)
+            await db.set_upload_as_doc(user_id, True)
         await Settings(update.message)
     elif update.data == "notifon":
-        notif = await db.get_notif(update.from_user.id)
+        notif = await db.get_notif(user_id)
         if notif:
             await update.answer("Bot bildirimleri kapatıldı.")
-            await db.set_notif(update.from_user.id, False)
+            await db.set_notif(user_id, False)
         else:
             await update.answer("Bot bildirimleri etkinleştirildi.")
-            await db.set_notif(update.from_user.id, True)
+            await db.set_notif(user_id, True)
         await Settings(update.message)
     elif "close" in update.data:
         await update.message.delete(True)
