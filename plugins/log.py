74c0ffe98d1cd46e06f65607c18408ccded2204e
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2022-04-30 23:03:18 +0300

    pyrogram update

--- plugins/log.py
+++ plugins/log.py
@@ -14,7 +14,7 @@ async def sendLogs(client, message):
     with open('log.txt', 'rb') as f:
         try:
             await client.send_document(document=f,
-                                       file_name=f.name, reply_to_message_id=message.message_id,
+                                       file_name=f.name, reply_to_message_id=message.id,
                                        chat_id=message.chat.id, caption=f.name)
         except Exception as e:
             await message.reply_text(str(e))
