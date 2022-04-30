78ffdd3b816670698c9fcf863f3450af3416204c
Trelicon <88194972+trelicon@users.noreply.github.com>
2022-05-01 02:14:37 +0300

    Update broadcast.py

--- database/broadcast.py
+++ database/broadcast.py
@@ -1,12 +1,16 @@
-import datetime, asyncio, time
+import asyncio
+import datetime
+import time
 
 from pyrogram import Client
-from database.database import db
-from pyrogram.types import Message
 from pyrogram.enums import ChatMemberStatus
-from pyrogram.errors import UserNotParticipant
-from config import BROADCAST_AS_COPY, LOGGER, AUTH_CHANNEL
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
+from pyrogram.errors import UserNotParticipant
+from pyrogram.types import Message
+
+from config import BROADCAST_AS_COPY, LOGGER, AUTH_CHANNEL
+from database.database import db
+
 
 async def broadcast_messages(bot, user_id, message):
     if AUTH_CHANNEL:
@@ -26,8 +30,8 @@ async def broadcast_messages(bot, user_id, message):
             await message.copy(chat_id=user_id, protect_content=True)
         return True, "Succes"
     except FloodWait as e:
-        await asyncio.sleep(e.x)
-        return await broadcast_messages(user_id, message)
+        await asyncio.sleep(e.value)
+        return await broadcast_messages(bot, user_id, message)
     except InputUserDeactivated:
         await db.delete_user(int(user_id))
         LOGGER.info(f"{user_id} - Hesap silindiği için veritabanından kaldırıldı.")
@@ -60,9 +64,9 @@ async def broadcast_handler(bot: Client, m: Message):
         pti, sh = await broadcast_messages(bot, int(user['id']), b_msg)
         if pti:
             success += 1
-        elif pti == False:
+        elif not pti:
             if sh == "Blocked":
-                blocked +=1
+                blocked += 1
             elif sh == "Deleted":
                 deleted += 1
             elif sh == "Error":
@@ -70,6 +74,17 @@ async def broadcast_handler(bot: Client, m: Message):
         done += 1
         await asyncio.sleep(2)
         if not done % 20:
-            await sts.edit(f"Yayın devam ediyor:\n\nToplam Kullanıcılar {total_users}\nTamamlanan: {done} / {total_users}\nBaşarılı: {success}\nEngellemiş: {blocked}\nSilmiş: {deleted}")
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Yayın Tamamlandı:\n{time_taken} saniye içinde tamamlandı.\n\nToplam Kullanıcılar {total_users}\nTamamlanan: {done} / {total_users}\nBaşarılı: {success}\nEngellemiş: {blocked}\nSilmiş: {deleted}")
+            await sts.edit(f"Yayın devam ediyor:\n\n"
+                           f"Toplam Kullanıcılar {total_users}\n"
+                           f"Tamamlanan: {done} / {total_users}\n"
+                           f"Başarılı: {success}\n"
+                           f"Engellemiş: {blocked}\n"
+                           f"Silmiş: {deleted}")
+    time_taken = datetime.timedelta(seconds=int(time.time() - start_time))
+    await sts.edit(f"Yayın Tamamlandı:\n"
+                   f"{time_taken} saniye içinde tamamlandı.\n\n"
+                   f"Toplam Kullanıcılar {total_users}\n"
+                   f"Tamamlanan: {done} / {total_users}\n"
+                   f"Başarılı: {success}\nEngellemiş: {blocked}\n"
+                   f"Silmiş: {deleted}"
+                   )
