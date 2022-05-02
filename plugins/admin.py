f984dba6e7fc8165ec32ccd351882b25de3fa996
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2022-05-02 14:46:35 +0300

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -35,65 +35,68 @@ async def status_handler(_, m: Message):
     text += f"**CPU Kullanımı:** `{cpu_usage}%` \n"
     text += f"**RAM Kullanımı:** `{ram_usage}%`\n\n"
     text += f"**DB'deki Toplam Kullanıcılar:** `{total_users}`"
-    if HEROKU_API_KEY is not None and HEROKU_APP_NAME is not None:
-        Heroku = heroku3.from_key(HEROKU_API_KEY)
-        app = Heroku.app(HEROKU_APP_NAME)
-    else:
-        await m.reply_text(
-            text=text,
-            quote=True
-        )
-        return
-
-    useragent = (
-        "Mozilla/5.0 (Linux; Android 10; SM-G975F) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/81.0.4044.117 Mobile Safari/537.36"
-    )
-    user_id = Heroku.account().id
-    headers = {
-        "User-Agent": useragent,
-        "Authorization": f"Bearer {HEROKU_API_KEY}",
-        "Accept": "application/vnd.heroku+json; version=3.account-quotas",
-    }
-    path = "/accounts/" + user_id + "/actions/get-quota"
-    session = requests.Session()
-    with session as ses:
-        with ses.get(heroku_api + path, headers=headers) as r:
-            result = r.json()
-            """Account Quota."""
-            quota = result["account_quota"]
-            quota_used = result["quota_used"]
-            quota_remain = quota - quota_used
-            quota_percent = math.floor(quota_remain / quota * 100)
-            minutes_remain = quota_remain / 60
-            hours = math.floor(minutes_remain / 60)
-            minutes = math.floor(minutes_remain % 60)
-            day = math.floor(hours / 24)
-
-            """App Quota."""
-            Apps = result["apps"]
-            for apps in Apps:
-                if apps.get("app_uuid") == app.id:
-                    AppQuotaUsed = apps.get("quota_used") / 60
-                    AppPercent = math.floor(apps.get("quota_used") * 100 / quota)
-                    break
-            else:
-                AppQuotaUsed = 0
-                AppPercent = 0
-
-            AppHours = math.floor(AppQuotaUsed / 60)
-            AppMinutes = math.floor(AppQuotaUsed % 60)
-
+    try:
+        if HEROKU_API_KEY is not None and HEROKU_APP_NAME is not None:
+            Heroku = heroku3.from_key(HEROKU_API_KEY)
+            app = Heroku.app(HEROKU_APP_NAME)
+        else:
             await m.reply_text(
-                f"**ℹ️ Dyno Kullanımı**\n\n`🟢 {app.name}`:\n"
-                f"• `{AppHours}` **Saat ve** `{AppMinutes}` **Dakika\n💯: {AppPercent}%**\n\n"
-                "**⚠️ Kalan Dyno**\n"
-                f"• `{hours}` **Saat ve** `{minutes}` **Dakika\n💯: {quota_percent}%**\n\n"
-                "**❌ Tahmini Kalan Süre**\n"
-                f"• `{day}` **Gün**" + '\n\n' + text,
+                text=text,
                 quote=True
             )
+            return
+
+        useragent = (
+            "Mozilla/5.0 (Linux; Android 10; SM-G975F) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/81.0.4044.117 Mobile Safari/537.36"
+        )
+        user_id = Heroku.account().id
+        headers = {
+            "User-Agent": useragent,
+            "Authorization": f"Bearer {HEROKU_API_KEY}",
+            "Accept": "application/vnd.heroku+json; version=3.account-quotas",
+        }
+        path = "/accounts/" + user_id + "/actions/get-quota"
+        session = requests.Session()
+        with session as ses:
+            with ses.get(heroku_api + path, headers=headers) as r:
+                result = r.json()
+                """Account Quota."""
+                quota = result["account_quota"]
+                quota_used = result["quota_used"]
+                quota_remain = quota - quota_used
+                quota_percent = math.floor(quota_remain / quota * 100)
+                minutes_remain = quota_remain / 60
+                hours = math.floor(minutes_remain / 60)
+                minutes = math.floor(minutes_remain % 60)
+                day = math.floor(hours / 24)
+
+                """App Quota."""
+                Apps = result["apps"]
+                for apps in Apps:
+                    if apps.get("app_uuid") == app.id:
+                        AppQuotaUsed = apps.get("quota_used") / 60
+                        AppPercent = math.floor(apps.get("quota_used") * 100 / quota)
+                        break
+                else:
+                    AppQuotaUsed = 0
+                    AppPercent = 0
+
+                AppHours = math.floor(AppQuotaUsed / 60)
+                AppMinutes = math.floor(AppQuotaUsed % 60)
+
+                await m.reply_text(
+                    f"**ℹ️ Dyno Kullanımı**\n\n`🟢 {app.name}`:\n"
+                    f"• `{AppHours}` **Saat ve** `{AppMinutes}` **Dakika\n💯: {AppPercent}%**\n\n"
+                    "**⚠️ Kalan Dyno**\n"
+                    f"• `{hours}` **Saat ve** `{minutes}` **Dakika\n💯: {quota_percent}%**\n\n"
+                    "**❌ Tahmini Kalan Süre**\n"
+                    f"• `{day}` **Gün**" + '\n\n' + text,
+                    quote=True
+                )
+    except Exception as e:
+        await m.reply_text(f"**Error:** `{e}`")
 
 
 @Client.on_message(filters.command("broadcast") & filters.user(OWNER_ID) & filters.reply)
